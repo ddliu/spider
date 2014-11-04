d829d5c03625cd25bc6c743a6b915d3a7d585ba0
dong <ddliuhb@gmail.com>
2014-11-04 08:49:37 +0800

    co

--- src/Scheduler.php
+++ src/Scheduler.php
@@ -1,6 +1,16 @@
 <?php
-namespace ddliu\Spider;
+namespace ddliu\spider;
 
 class Scheduler {
-    
+    public function add() {
+
+    }
+
+    public function fetch($n) {
+
+    }
+
+    public function remove() {
+
+    }
 }
--- src/Spider.php
+++ src/Spider.php
@@ -2,11 +2,20 @@
 namespace ddliu\spider;
 
 class Spider {
-    public function addTask() {
+    public function addTask($taskInfo) {
 
     }
 
     public function run() {
-        
+        while ($task = $this->scheduler->fetch()) {
+            $this->scheduler->remove($task)
+            foreach ($this->pipes as $pipe) {
+                $pipe($this, $task);
+            }
+        }
+    }
+
+    public function pipe($pipe) {
+        $this->pipes[] = $pipe;
     }
 }
